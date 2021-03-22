a05828bf831a7e7973e6599fa2e91ae2ce19edef
Hussein  Abdeljabbar Ismail Martínez <hussein.odeh@gmail.com>
2021-03-22 10:15:32 +0100

    Create config.h
    Config file, modify for each controller

---
+++ IoT/IRRIGATOR/config.h
@@ -0,0 +1,18 @@
+#define CONFIG_H
+
+//GENERAL SETTINGS
+const int upload_speed = 115200;
+const int digital_output_pin1 = 5;
+const char *name_pin1 = "Pump#1";
+
+//WIFI SETTINGS
+const char *wifi_ssid = "Codeworks";
+const char *wifi_password = "codinginthesun";
+
+//MQTT SETTINGS
+const char *mqtt_broker = "192.168.1.170";
+const int mqtt_port = 1883;
+const char *mqttId = "pump1";
+const char mqtt_inTopic[] = "actionTest";
+const char mqtt_outTopic[] = "statusTest";
+const char mqtt_resTopic[] = "responseTest";
