3f2b078374015ed8ca4d7c07154f8a4747b3fa1a
ascolm <mertascioglu@gmail.com>
2021-03-24 12:19:43 +0100

    build(db config): add option to choose between test/dev dbs

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 import { ActionModule } from './action/action.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Connection } from 'typeorm';
+import { Connection, getConnectionOptions } from 'typeorm';
 import { UsersModule } from './users/users.module';
 import { AreasModule } from './areas/areas.module';
 import { SensorsModule } from './sensors/sensors.module';
@@ -14,10 +14,13 @@ import { MqttService } from './mqtt/mqtt.service';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
+const dbName = process.env.NODE_ENV === 'test' ? 'test' : 'development';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useFactory: async () => await getConnectionOptions(dbName)
+    }),
     ActionModule,
     CronActionModule,
     UsersModule,
