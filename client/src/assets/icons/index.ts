4e87495b02ae0497189ec3d36b14f4418205d744
huss <hussein.odeh@gmail.com>
2021-03-24 15:26:41 +0100

    feat(fe): added svgs to workspace

--- client/src/assets/icons/index.ts
+++ client/src/assets/icons/index.ts
@@ -1,7 +1,10 @@
+import { ReactComponent as Logo } from './logo.svg';
+import { ReactComponent as IrrigatingPlant } from './irrigatingPlant.svg';
+import { ReactComponent as Plant } from './plant.svg';
 import { ReactComponent as Calendar } from './calendar.svg';
 import { ReactComponent as Chatting } from './chatting.svg';
 import { ReactComponent as Home } from './home.svg';
 import { ReactComponent as Image } from './image.svg';
 import { ReactComponent as User } from './user.svg';
 
-export { Calendar, Chatting, Home, Image, User };
+export { Logo, IrrigatingPlant, Plant, Calendar, Chatting, Home, Image, User };
