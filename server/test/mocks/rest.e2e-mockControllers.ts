a19c4a81f508711c83bcee2ed21f583e57e73ab1
ascolm <mertascioglu@gmail.com>
2021-03-24 19:57:16 +0100

    test(test mocks): create mocks for db seeding & rest API e2e tests

---
+++ server/test/mocks/rest.e2e-mockControllers.ts
@@ -0,0 +1,59 @@
+/* eslint-disable */
+
+export const mockControllers =
+[
+  {
+      "id": 1,
+      "area": {
+          "id": 1,
+          "isActive": true
+      },
+      "type": "TestiePump",
+      "isActive": true
+  },
+  {
+      "id": 2,
+      "area": {
+          "id": 1,
+          "isActive": true
+      },
+      "type": "TestieMusic",
+      "isActive": true
+  },
+  {
+      "id": 3,
+      "area": {
+          "id": 2,
+          "isActive": true
+      },
+      "type": "TestieDoor",
+      "isActive": true
+  },
+  {
+      "id": 4,
+      "area": {
+          "id": 3,
+          "isActive": true
+      },
+      "type": "RemovePump",
+      "isActive": true
+  },
+  {
+      "id": 5,
+      "area": {
+          "id": 4,
+          "isActive": true
+      },
+      "type": "DarthDoor",
+      "isActive": true
+  },
+  {
+      "id": 6,
+      "area": {
+          "id": 4,
+          "isActive": true
+      },
+      "type": "DarthPump",
+      "isActive": true
+  }
+]
---
+++ server/test/mocks/rest.e2e-mockUsers.ts
@@ -0,0 +1,121 @@
+/* eslint-disable */
+
+export const mockUsers =
+[
+    {
+        "id": 1,
+        "firstName": "Testie",
+        "lastName": "Testov",
+        "areas": [
+            {
+                "id": 1,
+                "sensors": [
+                    {
+                        "id": 2,
+                        "type": "TestieLight",
+                        "isActive": true
+                    },
+                    {
+                        "id": 1,
+                        "type": "TestieHumid",
+                        "isActive": true
+                    }
+                ],
+                "controllers": [
+                    {
+                        "id": 1,
+                        "type": "TestiePump",
+                        "isActive": true
+                    },
+                    {
+                        "id": 2,
+                        "type": "TestieMusic",
+                        "isActive": true
+                    }
+                ],
+                "isActive": true
+            },
+            {
+                "id": 2,
+                "sensors": [
+                    {
+                        "id": 3,
+                        "type": "TestieLight2",
+                        "isActive": true
+                    }
+                ],
+                "controllers": [
+                    {
+                        "id": 3,
+                        "type": "TestieDoor",
+                        "isActive": true
+                    }
+                ],
+                "isActive": true
+            }
+        ],
+        "isActive": true
+    },
+    {
+        "id": 2,
+        "firstName": "Deletisek",
+        "lastName": "Removo",
+        "areas": [
+            {
+                "id": 3,
+                "sensors": [
+                    {
+                        "id": 4,
+                        "type": "RemoveHumid",
+                        "isActive": true
+                    }
+                ],
+                "controllers": [
+                    {
+                        "id": 4,
+                        "type": "RemovePump",
+                        "isActive": true
+                    }
+                ],
+                "isActive": true
+            }
+        ],
+        "isActive": true
+    },
+    {
+        "id": 3,
+        "firstName": "Darth",
+        "lastName": "Later",
+        "areas": [
+            {
+                "id": 4,
+                "sensors": [
+                    {
+                        "id": 6,
+                        "type": "DarthLight",
+                        "isActive": true
+                    },
+                    {
+                        "id": 5,
+                        "type": "DarthHumid",
+                        "isActive": true
+                    }
+                ],
+                "controllers": [
+                    {
+                        "id": 5,
+                        "type": "DarthDoor",
+                        "isActive": true
+                    },
+                    {
+                        "id": 6,
+                        "type": "DarthPump",
+                        "isActive": true
+                    }
+                ],
+                "isActive": true
+            }
+        ],
+        "isActive": true
+    }
+]
