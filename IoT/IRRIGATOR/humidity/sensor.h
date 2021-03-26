fc33e57902d97ffd3c265297c59ed89a7874f3a0
huss <hussein.odeh@gmail.com>
2021-03-26 16:18:27 +0100

    feat(iot): ioT, added sensor reading functionality

---
+++ IoT/IRRIGATOR/humidity/sensor.h
@@ -0,0 +1,10 @@
+//SENSOR CALIBRATION
+const int dry = 740; //value for dry sensor
+const int wet = 297; //value for wet sensor
+const int sensorPin = A0;
+int getSensorReading()
+{
+  int sensorVal = analogRead(sensorPin);
+  Serial.println(sensorVal);
+  return map(sensorVal, wet, dry, 100, 0);
+}
