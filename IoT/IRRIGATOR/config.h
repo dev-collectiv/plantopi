3a936b94a41e4e83cecf190c2fb4cb719e32e6f9
huss <hussein.odeh@gmail.com>
2021-03-26 17:47:33 +0100

    fix(iot): minor fix to have a boolean for not publishing if there is no sensor

--- IoT/IRRIGATOR/config.h
+++ IoT/IRRIGATOR/config.h
@@ -9,12 +9,14 @@ const char *wifiPassword = "codinginthesun";
 //MQTT SETTINGS
 const char *mqttBroker = "nanopim4v2";
 const int mqttPort = 1883;
-const char *mqttControllerId = "pump2";
-const char *sensorId = "humidity2";
+const char *mqttControllerId = "pump1";
 const char *mqttControllerInTopic = "action";
 const char *mqttControllerOutTopic = "status";
 const char *mqttControllerResTopic = "response";
-const char *mqttSensorTopic = "sensors";
 
 unsigned long statusInterval = 2000;
+
+bool sensorActive = false;
 unsigned long sensorInterval = 1000;
+const char *sensorId = "humidity1";
+const char *mqttSensorTopic = "sensors";
