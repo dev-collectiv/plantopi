01b17ade8f82dde27e679bf246e7e73c3ca47488
jose luis lamas <69245960+JoseLLamasT@users.noreply.github.com>
2021-03-27 16:05:04 +0100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import styles from './App.module.scss';
 import React, { useEffect, useState } from 'react';
+import { SocketContext, socket } from 'context/socket';
 import NavBar from 'components/NavBar/NavBar';
 import Dashboard from 'components/Dashboard/Dashboard';
 import DetailBar from 'components/DetailBar/DetailBar';
@@ -46,12 +47,14 @@ const App: React.FC = () => {
   }, []);
 
   return (
-    <div className={styles.container}>
-      <NavBar />
-      <Dashboard />
-      <DetailBar />
-      <AreaPanel />
-    </div>
+    <SocketContext.Provider value={socket}>
+      <div className={styles.container}>
+        <NavBar />
+        <Dashboard />
+        <DetailBar />
+        <AreaPanel />
+      </div>
+    </SocketContext.Provider>
   );
 };
 
