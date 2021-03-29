7a7585d00559298120c2f25c73ff2b2d3e8dfd0e
cxgarcia <schlaubitzcristobal@gmail.com>
2021-03-29 17:43:48 +0200

    refactor(refactored areas): refactored areas to make them leaner

--- client/src/components/AreaPanel/AreaPanel.tsx
+++ client/src/components/AreaPanel/AreaPanel.tsx
@@ -9,37 +9,45 @@ import { useEffect, useState } from 'react';
 const AreaPanel: any = () => {
   const [areas, setAreas] = useState<IGetArea[]>([]);
   const [showAreaForm, setShowAreaForm] = useState<boolean>(false);
- 
+
   useEffect(() => {
     apiArea.getArea().then((area) => {
-      setAreas(area); 
+      setAreas(area);
     });
   }, []);
- 
-  const addingArea = (area: IAddArea) => {
+
+  const addArea = (area: IAddArea) => {
     apiArea.postArea(area).then((area) => {
-      setAreas((prevAreas: any ) => [...prevAreas, area]);
+      setAreas((prevAreas: any) => [...prevAreas, area]);
     });
-    
   };
-  
-  const deleteArea = (id:number) => {
+
+  const deleteArea = (id: number) => {
     apiArea.deleteArea(id);
-    let areasfiltered:IGetArea[]=[];
-    areas.map((el:IGetArea) => {
-      if (el.id !== id) areasfiltered.push(el);
+    const _areas = areas.filter((el: IGetArea) => {
+      return el.id !== id;
     });
-    setAreas(areasfiltered);
+    setAreas(_areas);
   };
+
+  function renderAreas() {
+    return areas.map((area) => {
+      return <AreasDisplay area={area} deleteArea={deleteArea} />;
+    });
+  }
+
   return (
     <div className={styles.container}>
-      <div className={styles.title}>
-        <h1>Areas</h1>
-        <button onClick={() => setShowAreaForm(!showAreaForm)}> + </button>
+      <div className={styles.panelTop}>
+        <h2 className={styles.panelTitle}>AREAS</h2>
       </div>
       <div className={styles.areaContainer}></div>
-      {areas && <AreasDisplay areas={areas} deleteArea={deleteArea} showAreaForm={showAreaForm}/>}
-      {showAreaForm && <AddArea addingArea={addingArea}  />}
+      {renderAreas()}
+      {showAreaForm && <AddArea addingArea={addArea} />}
+
+      <button onClick={() => setShowAreaForm(!showAreaForm)} className={styles.newAreaButton}>
+        NEW AREA
+      </button>
     </div>
   );
 };
--- client/src/components/AreaPanel/AreasDisplay/AreasDisplay.tsx
+++ client/src/components/AreaPanel/AreasDisplay/AreasDisplay.tsx
@@ -1,26 +1,16 @@
 import styles from './AreasDisplay.module.scss';
-import {IGetArea} from '../../../types/areaInterfaces';
-const AreasDisplay: any = ({ areas, deleteArea, showAreaForm }: any) => {
-   
-  let styleContainer;
-  if (showAreaForm) {
-    styleContainer = styles.componentAreaContainer;
-  } else {
-    styleContainer=styles.componentAreaContainer2;
-  }
-  if (areas) {
-    return (
-      <div className={styleContainer}>
-        {areas.map((area: IGetArea) => (
-          <div className={styles.areasContainer} key={areas.id}>
-            <h2>Area id: {area.id}</h2>
-            <h2>Area id: {area.isActive}</h2>
-            <h2>Area id: {area.id}</h2>
-            <button onClick={() => deleteArea(area.id)}>delete </button>
-          </div>
-        ))} 
+import { IGetArea } from 'types/areaInterfaces';
+
+const AreasDisplay: React.FC<{ area: IGetArea; deleteArea: Function }> = ({ area, deleteArea }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.areaContainer} key={area.id}>
+        <h2>Area id: {area.id}</h2>
+        <h2>Area sensors: {area.isActive}</h2>
+        <h2>Area id: {area.id}</h2>
+        <button onClick={() => deleteArea(area.id)}>DELETE</button>
       </div>
-    ); 
-  }
+    </div>
+  );
 };
 export default AreasDisplay;
