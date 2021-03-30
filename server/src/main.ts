2073910f7738afd8c9728b652bb59b40a1dfe7c1
ascolm <mertascioglu@gmail.com>
2021-03-30 20:02:48 +0200

    refactor(db seeds): separate db seeds to another function

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,21 +8,26 @@ import { Controller } from './controllers/entities/controller.entity';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const connection = app.get(Connection);
+  function dbSeeds () {
+    return new Promise (async res => {
+      const connection = app.get(Connection);
 
-  function createSeeds (tableName: string, seedData: any) {
-    return connection.createQueryBuilder().insert().into(tableName).values(seedData).execute();
+      function createSeeds (tableName: string, seedData: any) {
+        return connection.createQueryBuilder().insert().into(tableName).values(seedData).execute();
+      }
+
+      await createSeeds('user', demoUserSeed);
+      await createSeeds('area', demoAreaSeed);
+      const demoController = await connection.getRepository(Controller).create(demoControllerSeed);
+      await connection.getRepository(Controller).save(demoController);
+
+      const demoSensor = await connection.getRepository(Sensor).create(demoSensorSeed);
+      await connection.getRepository(Sensor).save(demoSensor);
+      res('');
+    });
   }
 
-  await createSeeds('user', demoUserSeed);
-  await createSeeds('area', demoAreaSeed);
-
-  const demoController = await connection.getRepository(Controller).create(demoControllerSeed);
-  await connection.getRepository(Controller).save(demoController);
-
-  const demoSensor = await connection.getRepository(Sensor).create(demoSensorSeed);
-  await connection.getRepository(Sensor).save(demoSensor);
-
+  await dbSeeds();
 
   app.enableCors();
   const port = process.env.HTTP_PORT || 3001;
@@ -31,3 +36,4 @@ async function bootstrap() {
   await app.listen(port, host);
 }
 bootstrap();
+
