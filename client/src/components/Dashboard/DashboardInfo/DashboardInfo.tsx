a44a1ca6b9b8fb29cb7717884d41d04b971a6e82
Jose Lamas <jllt1992@gmail.com>
2021-03-30 17:27:47 +0200

    feat: areaSelected is on the dashboard

--- client/src/components/Dashboard/DashboardInfo/DashboardInfo.tsx
+++ client/src/components/Dashboard/DashboardInfo/DashboardInfo.tsx
@@ -14,16 +14,14 @@ import { SocketContext, socket } from 'context/socket';
 import { ISensorReading } from 'types/sensorsInterfaces';
 
 
-interface Props{
-  area: IGetArea | undefined
-}
 
-const DashboardInfo: React.FC<Props> = ({ area }) => {
+
+const DashboardInfo: React.FC<{selectedArea: IGetArea}> = ({ selectedArea }) => {
   let [currentWeather, setCurrentWeather] = useState<ICurrentWeather>();
   let [currentHumidity, setCurrentHumidity] = useState<number>(0);
 
   // AREA ID HARDCODED IN CURRENT WEATHER FETCH BELOW - SWAP WITH AREAID LATER
-
+  console.log('selectedArea',selectedArea );
   useEffect(() => {
     const initializeWeather = async () => {
       const weather = await fetchCurrentWeather('1');
@@ -41,6 +39,11 @@ const DashboardInfo: React.FC<Props> = ({ area }) => {
         {/*TODO <SensorCard id={area.sensors[0].id} name={area.sensors[0].name} type={area.sensors[0].type} reading={area.sensors[0].value} /> */}
         <SensorCard id="1" name="Humidity" type="humidity" reading={currentHumidity}/>
         {/*TODO <SensorCard id={area.sensors[1].id} name={area.sensors[1].name} type={area.sensors[1].type} reading={area.sensors[1].value} /> */}
+
+
+        { selectedArea && <h1>{selectedArea.name}</h1>}
+
+        
         <SensorCard id="2" name="Temperature" type="temperature" reading={Math.round(currentWeather?.current.temp)} />
       </TopCard>
       <BottomCard>
