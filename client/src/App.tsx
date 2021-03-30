cc1011ee97d6155619d55953d633019710496f0a
cxgarcia <schlaubitzcristobal@gmail.com>
2021-03-30 15:58:40 +0200

    feat(intro animation): intro animation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,22 @@
-import styles from './App.module.scss';
-import React from 'react';
+import React, { useState } from 'react';
 import NavBar from 'components/NavBar/NavBar';
 import Dashboard from 'components/Dashboard/Dashboard';
 
+import styles from './App.module.scss';
+import IntroAnimation from 'components/IntroAnimation/IntroAnimation';
+
 const App: React.FC = () => {
-  return (
-    <div className={styles.container}>
-      <NavBar />
-      <Dashboard />
-    </div>
-  );
+  const [isEndOfIntroAnimation, setIsEndOfIntroAnimation] = useState<boolean>(false);
+
+  if (!isEndOfIntroAnimation) {
+    return <IntroAnimation setIsEndOfIntroAnimation={setIsEndOfIntroAnimation} />;
+  } else
+    return (
+      <div className={styles.container}>
+        <NavBar />
+        <Dashboard />
+      </div>
+    );
 };
 
 export default App;
