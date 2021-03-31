9159aee0b49b2bef7053c35aa5027077210d10b9
cxgarcia <schlaubitzcristobal@gmail.com>
2021-03-31 09:45:11 +0200

    feat(add area styles): add area styles

--- client/src/components/AreaPanel/AddArea/AddArea.tsx
+++ client/src/components/AreaPanel/AddArea/AddArea.tsx
@@ -2,13 +2,13 @@ import { useState } from 'react';
 import { IAddArea } from 'types/areaInterfaces';
 import styles from './AddArea.module.scss';
 
-const AddArea: React.FC<{ addArea: Function;  cancelCreateArea:Function }> = ({ addArea, cancelCreateArea}) => {
+const AddArea: React.FC<{ addArea: Function; cancelCreateArea: Function }> = ({ addArea, cancelCreateArea }) => {
   const [newArea, setNewArea] = useState<IAddArea>({
     name: '',
     isActive: false,
     sensors: []
   });
-  
+
   const handleEvent = (event: React.ChangeEvent<HTMLInputElement>) => {
     setNewArea({
       ...newArea,
@@ -18,25 +18,33 @@ const AddArea: React.FC<{ addArea: Function;  cancelCreateArea:Function }> = ({ 
 
   return (
     <div className={styles.container}>
-      <h4>New Area</h4>
-      <label>
-        <h3> Name:</h3>
-        <input className={styles.input} value={newArea.name} name="name" type="text" onChange={handleEvent} />
+      <h3 className={styles.cardTitle}>New Area</h3>
+      <label className={styles.inputContainer}>
+        <h3> Name</h3>
+        <input value={newArea.name} name="name" type="text" onChange={handleEvent} />
       </label>
-      <label>
-        <h3> Longitude:</h3>
-        <input className={styles.input} value={newArea.longitude} name="longitude" type="text" onChange={handleEvent} />
+      <label className={styles.inputContainer}>
+        <h3> Longitude</h3>
+        <input value={newArea.longitude} name="longitude" type="text" onChange={handleEvent} />
       </label>
-      <label>
-        <h3> Latitude:</h3>
-        <input className={styles.input} value={newArea.latitude} name="latitude" type="text" onChange={handleEvent} />
+      <label className={styles.inputContainer}>
+        <h3> Latitude</h3>
+        <input value={newArea.latitude} name="latitude" type="text" onChange={handleEvent} />
       </label>
-      <div>
-        <button className={styles.btn} type="submit" onClick={() => {addArea(newArea); cancelCreateArea();} }>
-          CREATE
-        </button>
+
+      <div className={styles.buttonContainer}>
         <button className={styles.btn} type="submit" onClick={() => cancelCreateArea()}>
-          Cancel
+          CANCEL
+        </button>
+        <button
+          className={styles.btn}
+          type="submit"
+          onClick={() => {
+            addArea(newArea);
+            cancelCreateArea();
+          }}
+        >
+          CREATE
         </button>
       </div>
     </div>
